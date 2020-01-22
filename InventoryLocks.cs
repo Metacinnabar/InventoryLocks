9f6482bfa12519f0bab9e4519dc983e0f654fff9
charlie brannan <charliebrannan154@gmail.com>
2020-01-23 10:16:26 +1100

    Added comments everywhere and reformatted

--- InventoryLocks.cs
+++ InventoryLocks.cs
@@ -12,14 +12,18 @@ namespace InventoryLocks
 {
 	internal class InventoryLocks : Mod
 	{
+		//Get ItemSlot.canFavoriteAt via reflection, get its value then cast to its type
 		private bool[] CanFavoriteAt => (bool[])typeof(ItemSlot).GetField("canFavoriteAt", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
 
+		//Get ItemSlot.OverrideLeftClick with reflection
 		private MethodInfo MethodInfo_ItemSlot_OverrideLeftClick => typeof(ItemSlot).GetMethod("OverrideLeftClick", BindingFlags.NonPublic | BindingFlags.Static);
 
+		//Get ItemsSlot.SellOrTrash with reflection
 		private MethodInfo MethodInfo_ItemSlot_SellOrTrash => typeof(ItemSlot).GetMethod("SellOrTrash", BindingFlags.NonPublic | BindingFlags.Static);
 
 		public override void Load()
 		{
+			//Subscribe to On. hooks
 			On.Terraria.UI.ItemSlot.OverrideHover += ItemSlot_OverrideHover;
 			On.Terraria.UI.ItemSlot.OverrideLeftClick += ItemSlot_OverrideLeftClick;
 			On.Terraria.UI.ItemSlot.LeftClick_ItemArray_int_int += ItemSlot_LeftClick;
@@ -27,6 +31,7 @@ namespace InventoryLocks
 
 		public override void Unload()
 		{
+			//Unsubscribe to On. hooks
 			On.Terraria.UI.ItemSlot.OverrideHover -= ItemSlot_OverrideHover;
 			On.Terraria.UI.ItemSlot.OverrideLeftClick -= ItemSlot_OverrideLeftClick;
 			On.Terraria.UI.ItemSlot.LeftClick_ItemArray_int_int -= ItemSlot_LeftClick;
@@ -34,17 +39,21 @@ namespace InventoryLocks
 
 		private void ItemSlot_LeftClick(On.Terraria.UI.ItemSlot.orig_LeftClick_ItemArray_int_int orig, Item[] inv, int context, int slot)
 		{
+			//Invoke ItemSlot.OverrideLeftClick and save it as a varible to check for later
 			var OverrideLeftClick = MethodInfo_ItemSlot_OverrideLeftClick.Invoke(null, new object[3]
 			{
 				inv, context, slot
 			});
+
+			//Check if ItemSlot.OverrideLeftClick returns true, if so return
 			if ((bool)OverrideLeftClick)
-			{
 				return;
-			}
+
 			inv[slot].newAndShiny = false;
+
 			Player player = Main.player[Main.myPlayer];
 			bool flag = false;
+
 			switch (context)
 			{
 				case 0:
@@ -55,27 +64,30 @@ namespace InventoryLocks
 					flag = player.chest == -1;
 					break;
 			}
+
 			if (ItemSlot.ShiftInUse && flag)
 			{
+				//Invoke the ItemSlot.SellOrTrash method
 				MethodInfo_ItemSlot_SellOrTrash.Invoke(null, new object[3]
 				{
 					inv, context, slot
 				});
 				return;
 			}
+
 			if (player.itemAnimation == 0 && player.itemTime == 0)
 			{
 				int num = ItemSlot.PickItemMovementAction(inv, context, slot, Main.mouseItem);
+
 				if (num == 0)
 				{
 					if (context == 6 && Main.mouseItem.type != 0)
-					{
 						inv[slot].SetDefaults(0, false);
-					}
+
+					//Check if the inventory isnt the lock. If not, procide with the pickup
 					if (Main.LocalPlayer.inventory[slot].type != ModContent.ItemType<ItemLock>())
-					{
 						Utils.Swap(ref inv[slot], ref Main.mouseItem);
-					}
+
 					if (inv[slot].stack > 0)
 					{
 						if (context != 0)
@@ -94,14 +106,12 @@ namespace InventoryLocks
 							}
 						}
 						else
-						{
 							AchievementsHelper.NotifyItemPickup(player, inv[slot]);
-						}
 					}
+
 					if (inv[slot].type == 0 || inv[slot].stack < 1)
-					{
 						inv[slot] = new Item();
-					}
+
 					if (Main.mouseItem.IsTheSameAs(inv[slot]))
 					{
 						Utils.Swap(ref inv[slot].favorited, ref Main.mouseItem.favorited);
@@ -120,29 +130,28 @@ namespace InventoryLocks
 							}
 						}
 					}
+
 					if (Main.mouseItem.type == 0 || Main.mouseItem.stack < 1)
-					{
 						Main.mouseItem = new Item();
-					}
+
 					if (Main.mouseItem.type > 0 || inv[slot].type > 0)
 					{
 						Recipe.FindRecipes();
+
 						if (Main.LocalPlayer.inventory[slot].type != ModContent.ItemType<ItemLock>())
-						{
 							Main.PlaySound(7, -1, -1, 1, 1f, 0f);
-						}
 					}
+
 					if (context == 3 && Main.netMode == 1)
-					{
-						NetMessage.SendData(32, -1, -1, null, player.chest, (float)slot, 0f, 0f, 0, 0, 0);
-					}
+						NetMessage.SendData(32, -1, -1, null, player.chest, slot, 0f, 0f, 0, 0, 0);
 				}
 				else if (num == 1)
 				{
 					if (Main.mouseItem.stack == 1 && Main.mouseItem.type > 0 && inv[slot].type > 0 && inv[slot].IsNotTheSameAs(Main.mouseItem))
 					{
-						Utils.Swap<Item>(ref inv[slot], ref Main.mouseItem);
+						Utils.Swap(ref inv[slot], ref Main.mouseItem);
 						Main.PlaySound(7, -1, -1, 1, 1f, 0f);
+
 						if (inv[slot].stack > 0)
 						{
 							if (context != 0)
@@ -161,22 +170,19 @@ namespace InventoryLocks
 								}
 							}
 							else
-							{
 								AchievementsHelper.NotifyItemPickup(player, inv[slot]);
-							}
 						}
 					}
 					else if (Main.mouseItem.type == 0 && inv[slot].type > 0)
 					{
-						Utils.Swap<Item>(ref inv[slot], ref Main.mouseItem);
+						Utils.Swap(ref inv[slot], ref Main.mouseItem);
+
 						if (inv[slot].type == 0 || inv[slot].stack < 1)
-						{
 							inv[slot] = new Item();
-						}
+
 						if (Main.mouseItem.type == 0 || Main.mouseItem.stack < 1)
-						{
 							Main.mouseItem = new Item();
-						}
+
 						if (Main.mouseItem.type > 0 || inv[slot].type > 0)
 						{
 							Recipe.FindRecipes();
@@ -187,15 +193,14 @@ namespace InventoryLocks
 					{
 						if (Main.mouseItem.stack == 1)
 						{
-							Utils.Swap<Item>(ref inv[slot], ref Main.mouseItem);
+							Utils.Swap(ref inv[slot], ref Main.mouseItem);
+
 							if (inv[slot].type == 0 || inv[slot].stack < 1)
-							{
 								inv[slot] = new Item();
-							}
+
 							if (Main.mouseItem.type == 0 || Main.mouseItem.stack < 1)
-							{
 								Main.mouseItem = new Item();
-							}
+
 							if (Main.mouseItem.type > 0 || inv[slot].type > 0)
 							{
 								Recipe.FindRecipes();
@@ -209,6 +214,7 @@ namespace InventoryLocks
 							Recipe.FindRecipes();
 							Main.PlaySound(7, -1, -1, 1, 1f, 0f);
 						}
+
 						if (inv[slot].stack > 0)
 						{
 							if (context != 0)
@@ -227,9 +233,7 @@ namespace InventoryLocks
 								}
 							}
 							else
-							{
 								AchievementsHelper.NotifyItemPickup(player, inv[slot]);
-							}
 						}
 					}
 				}
@@ -257,22 +261,19 @@ namespace InventoryLocks
 								}
 							}
 							else
-							{
 								AchievementsHelper.NotifyItemPickup(player, inv[slot]);
-							}
 						}
 					}
 					else if (Main.mouseItem.type == 0 && inv[slot].type > 0)
 					{
 						Utils.Swap(ref inv[slot], ref Main.mouseItem);
+
 						if (inv[slot].type == 0 || inv[slot].stack < 1)
-						{
 							inv[slot] = new Item();
-						}
+
 						if (Main.mouseItem.type == 0 || Main.mouseItem.stack < 1)
-						{
 							Main.mouseItem = new Item();
-						}
+
 						if (Main.mouseItem.type > 0 || inv[slot].type > 0)
 						{
 							Recipe.FindRecipes();
@@ -284,14 +285,13 @@ namespace InventoryLocks
 						if (Main.mouseItem.stack == 1)
 						{
 							Utils.Swap(ref inv[slot], ref Main.mouseItem);
+
 							if (inv[slot].type == 0 || inv[slot].stack < 1)
-							{
 								inv[slot] = new Item();
-							}
+
 							if (Main.mouseItem.type == 0 || Main.mouseItem.stack < 1)
-							{
 								Main.mouseItem = new Item();
-							}
+
 							if (Main.mouseItem.type > 0 || inv[slot].type > 0)
 							{
 								Recipe.FindRecipes();
@@ -305,6 +305,7 @@ namespace InventoryLocks
 							Recipe.FindRecipes();
 							Main.PlaySound(7, -1, -1, 1, 1f, 0f);
 						}
+
 						if (inv[slot].stack > 0)
 						{
 							if (context != 0)
@@ -323,9 +324,7 @@ namespace InventoryLocks
 								}
 							}
 							else
-							{
 								AchievementsHelper.NotifyItemPickup(player, inv[slot]);
-							}
 						}
 					}
 				}
@@ -333,33 +332,29 @@ namespace InventoryLocks
 				{
 					Main.mouseItem = inv[slot].Clone();
 					Main.mouseItem.stack = 1;
+
 					if (inv[slot].buyOnce)
-					{
 						Main.mouseItem.value *= 5;
-					}
 					else
-					{
 						Main.mouseItem.Prefix(-1);
-					}
-					Main.mouseItem.position = player.Center - new Vector2((float)Main.mouseItem.width, (float)Main.mouseItem.headSlot) / 2f;
+
+					Main.mouseItem.position = player.Center - new Vector2(Main.mouseItem.width, Main.mouseItem.headSlot) / 2f;
 					ItemText.NewText(Main.mouseItem, Main.mouseItem.stack, false, false);
+
 					if (inv[slot].buyOnce && --inv[slot].stack <= 0)
-					{
 						inv[slot].SetDefaults(0, false);
-					}
+
 					if (inv[slot].value > 0)
-					{
 						Main.PlaySound(18, -1, -1, 1, 1f, 0f);
-					}
 					else
-					{
 						Main.PlaySound(7, -1, -1, 1, 1f, 0f);
-					}
+
 					PlayerHooks.PostBuyItem(player, Main.npc[player.talkNPC], inv, Main.mouseItem);
 				}
 				else if (num == 4 && PlayerHooks.CanSellItem(player, Main.npc[player.talkNPC], inv, Main.mouseItem))
 				{
 					Chest chest = Main.instance.shop[Main.npcShop];
+
 					if (player.SellItem(Main.mouseItem.value, Main.mouseItem.stack))
 					{
 						int soldItemIndex = chest.AddShop(Main.mouseItem);
@@ -374,6 +369,7 @@ namespace InventoryLocks
 						Main.PlaySound(7, -1, -1, 1, 1f, 0f);
 						PlayerHooks.PostSellItem(player, Main.npc[player.talkNPC], chest.item, chest.item[soldItemIndex]);
 					}
+
 					Recipe.FindRecipes();
 				}
 				switch (context)
@@ -395,10 +391,15 @@ namespace InventoryLocks
 		private bool ItemSlot_OverrideLeftClick(On.Terraria.UI.ItemSlot.orig_OverrideLeftClick orig, Item[] inv, int context, int slot)
 		{
 			Item item = inv[slot];
+
+			//Check if the slothas no item in it and the favorite key is down
 			if (item.IsAir && Main.keyState.IsKeyDown(Main.FavoriteKey))
 			{
+				//If true, add lock to inventory
 				item.SetDefaults(ModContent.ItemType<ItemLock>());
+				//Favorite lock
 				item.favorited = true;
+				//Play sound
 				Main.PlaySound(SoundID.Unlock);
 				return true;
 			}
@@ -406,17 +407,16 @@ namespace InventoryLocks
 			if (Main.cursorOverride == 2)
 			{
 				if (ChatManager.AddChatText(Main.fontMouseText, ItemTagHandler.GenerateTag(item), Vector2.One))
-				{
 					Main.PlaySound(12, -1, -1, 1, 1f, 0f);
-				}
+
 				return true;
 			}
+
 			if (Main.cursorOverride == 3)
 			{
 				if (!CanFavoriteAt[context])
-				{
 					return false;
-				}
+
 				item.favorited = !item.favorited;
 				Main.PlaySound(12, -1, -1, 1, 1f, 0f);
 				return true;
@@ -429,20 +429,23 @@ namespace InventoryLocks
 					Main.PlaySound(12, -1, -1, 1, 1f, 0f);
 					return true;
 				}
+
 				if (Main.cursorOverride == 8)
 				{
 					inv[slot] = Main.player[Main.myPlayer].GetItem(Main.myPlayer, inv[slot], false, true);
+
 					if (Main.player[Main.myPlayer].chest > -1)
-					{
 						NetMessage.SendData(32, -1, -1, null, Main.player[Main.myPlayer].chest, slot, 0f, 0f, 0, 0, 0);
-					}
+
 					return true;
 				}
+
 				if (Main.cursorOverride == 9)
 				{
 					ChestUI.TryPlacingInChest(inv[slot], false);
 					return true;
 				}
+
 				return false;
 			}
 		}
@@ -458,27 +461,21 @@ namespace InventoryLocks
 					case 1:
 					case 2:
 						if (Main.npcShop > 0 && !item.favorited)
-						{
 							Main.cursorOverride = 10;
-						}
+
 						else if (Main.player[Main.myPlayer].chest != -1)
 						{
 							if (ChestUI.TryPlacingInChest(item, true))
-							{
 								Main.cursorOverride = 9;
-							}
 						}
+
 						else
-						{
 							Main.cursorOverride = 6;
-						}
 						break;
 					case 3:
 					case 4:
 						if (Main.player[Main.myPlayer].ItemSpace(item))
-						{
 							Main.cursorOverride = 8;
-						}
 						break;
 					case 5:
 					case 8:
@@ -492,16 +489,16 @@ namespace InventoryLocks
 					case 19:
 					case 20:
 						if (Main.player[Main.myPlayer].ItemSpace(inv[slot]))
-						{
 							Main.cursorOverride = 7;
-						}
 						break;
 				}
 			}
+
+			//Check if the slot has an item in it and the favorite key is down
 			if (item.IsAir && Main.keyState.IsKeyDown(Main.FavoriteKey))
-			{
+				//Draw favorite cursor
 				Main.cursorOverride = 3;
-			}
+
 			if (Main.keyState.IsKeyDown(Main.FavoriteKey) && CanFavoriteAt[context])
 			{
 				if (item.type > 0 && item.stack > 0 && Main.drawingPlayerChat)
@@ -509,10 +506,9 @@ namespace InventoryLocks
 					Main.cursorOverride = 2;
 					return;
 				}
+
 				if (item.type > 0 && item.stack > 0)
-				{
 					Main.cursorOverride = 3;
-				}
 			}
 		}
 	}
@@ -520,13 +516,14 @@ namespace InventoryLocks
 	internal class ItemLock : ModItem
 	{
 		public override void Update(ref float gravity, ref float maxFallSpeed)
-		{
-			item.active = false;
-		}
+			//When in world, set active to false to remove the item from the world
+			=> item.active = false;
 
 		public override void UpdateInventory(Player player)
 		{
+			//Check if the item is favorited
 			if (!item.favorited)
+				//If its not, rremove the item from inventory
 				item.TurnToAir();
 		}
 	}
